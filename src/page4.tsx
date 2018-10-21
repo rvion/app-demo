98f792a4a4af601a1182b96df40f251c804b1136
Amelie Juge <amelie.juge@hec.edu>
2018-10-21 23:30:07 +0200

    tweak page4 content

--- src/page4.tsx
+++ src/page4.tsx
@@ -4,25 +4,12 @@ import { observer } from 'mobx-react'
 
 @observer
 export class Page4 extends React.Component {
-    constructor(props) {
-        super(props)
-        if (store.beards.length === 0) store.fetchBoards()
-    }
-
     render() {
         return (
             <div>
-                <h2>Barbes</h2>
-                <button onClick={() => store.fetchBoards()}>Fetch</button>
-                <ul>
-                    {store.beards.map(beard => {
-                        return (
-                            <li key={beard._id}>
-                                <pre>{JSON.stringify(beard, null, 4)}</pre>
-                            </li>
-                        )
-                    })}
-                </ul>
+                <h2>New challenge</h2>
+                <h3>Select a friend</h3>
+                function that fetches friends list
             </div>
         )
     }
